0c14edbd1f2e55ed147bfb38765b4f6ef0a3390b
iamcodex96 <33327804+iamcodex96@users.noreply.github.com>
2018-06-03 15:14:08 +0200

    Poblado

--- New Unity Project/Assets/_Retse/Scripts_iniciales/Interface-Scripts/SetSunLight.cs
+++ New Unity Project/Assets/_Retse/Scripts_iniciales/Interface-Scripts/SetSunLight.cs
@@ -28,33 +28,7 @@ public class SetSunLight : MonoBehaviour
     {
 
         stars.transform.rotation = transform.rotation;
-        /*
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-
-            lighton = !lighton;
-
-        }
-
-
-        if (lighton)
-        {
-            Color final = Color.white * Mathf.LinearToGammaSpace(5);
-            lightwall.material.SetColor("_EmissionColor", final);
-            DynamicGI.SetEmissive(lightwall, final);
-        }
-        else
-        {
-            Color final = Color.white * Mathf.LinearToGammaSpace(0);
-            lightwall.material.SetColor("_EmissionColor", final);
-            DynamicGI.SetEmissive(lightwall, final);
-        }
-        */
-        //Vector3 tvec = Camera.main.transform.position;
-        //worldProbe.transform.position = tvec;
-
-        //water.material.mainTextureOffset = new Vector2(Time.time / 100, 0);
-        //water.material.SetTextureOffset("_DetailAlbedoMap", new Vector2(0, Time.time / 80));
+   
 
     }
 }
