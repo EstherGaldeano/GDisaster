de2342dff759154a849f78719e50bd9fa07e10fd
Sergio Esteve <33327507+ookami34@users.noreply.github.com>
2018-06-06 16:01:21 +0200

    inventory

---
+++ New Unity Project/Assets/_Retse/Scripts_iniciales/Wiki/WikiRegisters.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WikiRegisters : MonoBehaviour {
+
+    public static WikiRegisters instance;
+
+    public Monster m1;
+    public Monster m2;
+    public Monster m3;
+    public Monster m4;
+    public Monster m5;
+    public Monster m6;
+    public Monster m7;
+    public Monster m8;
+    public Monster m9;
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+    public List<Monster> monsters = new List<Monster>();
+
+    void Awake () {
+        if (instance != null)
+        {
+            //Debug.LogWarning("More than one instance of Inventory found!");
+            return;
+        }
+        instance = this;
+        
+    }
+
+    public void Add()
+    {
+        if(m1 != null)
+        {
+            monsters.Add(m1);
+        }
+
+        if (m2 != null)
+        {
+            monsters.Add(m2);
+        }
+
+        if (m3 != null)
+        {
+            monsters.Add(m3);
+        }
+
+        if (m4 != null)
+        {
+            monsters.Add(m4);
+        }
+
+        if (m5 != null)
+        {
+            monsters.Add(m5);
+        }
+
+        if (m6 != null)
+        {
+            monsters.Add(m6);
+        }
+
+        if (m7 != null)
+        {
+            monsters.Add(m7);
+        }
+
+        if (m8 != null)
+        {
+            monsters.Add(m8);
+        }
+
+        if (m9 != null)
+        {
+            monsters.Add(m9);
+        }
+    }
+
+}
---
+++ New Unity Project/Assets/_Retse/Scripts_iniciales/Wiki/WikiUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WikiUI : MonoBehaviour {
+
+    public Transform itemsParent;
+
+    WikiRegisters wiki;
+
+    WikiSlot[] slots;
+    // Use this for initialization
+    void Start()
+    {
+        wiki = WikiRegisters.instance;
+        wiki.onItemChangedCallback += UpdateUI;
+
+        slots = itemsParent.GetComponentsInChildren<WikiSlot>();
+    }
+
+    void UpdateUI()
+    {
+        Debug.Log("holaMonster");
+
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < wiki.monsters.Count)
+            {
+                slots[i].AddMonster(wiki.monsters[i]);
+            }
+            else
+            {
+                slots[i].ClearSlot();
+            }
+        }
+    }
+}
